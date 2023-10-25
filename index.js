85b3edaa3916f24582b0b07725f2c66783a52638
Shahan Ameen <Shahanameen@gmail.com>
2023-10-25 17:10:40 -0500

    addedfiles

---
+++ index.js
@@ -0,0 +1,52 @@
+const inquirer = require("inquirer");
+const SVG = require("./lib/svg.js");
+const { Circle, Triangle, Square } = require("./lib/shapes.js");
+const fs = require("fs");
+
+class CLI {
+  run() {
+    return inquirer
+      .prompt([
+        {
+          name: "text",
+          type: "input",
+          message:
+            "Enter a 3 character text for the logo.",
+          
+        },
+        {
+          name: "textColor",
+          type: "input",
+          message: "Enter a color for the logo text",
+        },
+        {
+          name: "shapeType",
+          type: "list",
+          message: "Select a shape for the logo",
+          choices: ["circle", "square", "triangle"],
+        },
+        {
+          name: "shapeColor",
+          type: "input",
+          message: "Enter a color for the shape background",
+        },
+      ])
+      .then((response) => {
+        let shape;
+      if(response.shapeType === "circle"){
+        shape = new Circle()
+      }
+      else if(response.shapeType === "square"){
+        shape = new Square()
+      }
+      else if(response.shapeType === "triangle"){
+        shape = new Triangle()
+      }
+       // wire class methods here together here
+        return fs.writeFileSync("newlogo.svg", svg.render());
+      })
+      .then(() => {
+        console.log("Generated logo!");
+      })
+  }
+}
